ec32216e7639aaa4910d6e4fef24848a92988524
YuraRov <yura.rov555@gmail.com>
2022-09-06 12:39:48 +0300

    Finished tasks 1-6
    However, I still have questions to the 5-th task

---
+++ lab-1/rovinskyi_tasks_1_6.py
@@ -0,0 +1,18 @@
+# Task 1 - Read the integer number x from the keyboard 
+x = int(input("Please enter any integer: "))
+
+# Task 2 - Print x
+print(x)
+
+# Task 3 - Add 0.8 to x, convert it to integer type and print
+print(int(x + 0.8))
+
+# Task 4 - Convert x to string and print 'x*3'
+x = str(x)
+print(x*3)
+
+# Task 5 - Print a variable with the sep and end parameter for print function ???
+# print((x*3)[::1], sep='; ', end='.\n')
+
+# Task 6 - Print data with f-string
+print(f'{x = }')
