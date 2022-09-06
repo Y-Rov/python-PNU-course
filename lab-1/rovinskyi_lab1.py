2907f11cdbcf5a397965ee25a82c4a45eac17b3c
YuraRov <yura.rov555@gmail.com>
2022-09-06 18:34:18 +0300

    Rewrited the previous solution of task №7

--- lab-1/rovinskyi_lab1.py
+++ lab-1/rovinskyi_lab1.py
@@ -1,13 +1,22 @@
 # Task 7 - For the given number n < 100, complete the phrase "Zalik zdalo..."
 # with the sentences "n studentiv", "n studenty" or "n student",
-# using the correct endings
+# using the correct results
 
-n = int(input('Enter the quantity of students: '))
+n = input("Enter the quantity of students: ")
 phrase = "Zalik zdalo"
 
-if n % 10 >= 5 or n % 10 == 0 or 11 <= n <= 14:
-    print(f"{phrase} {n} studentiv")
-elif 2 <= n % 10 <= 4:
-    print(f"{phrase} {n} studenty")
-else:
-    print(f"{phrase} {n} student")
+if n.isnumeric():
+    if n[-1] in "234":
+        if len(n) > 1 and n[-2] == "1":
+            result = f"{phrase} {n} studentiv"
+        else:
+            result = f"{phrase} {n} studenty"
+    elif n[-1] == "1":
+        if len(n) > 1 and n[-2] != "1" or n == "1":
+            result = f"{phrase} {n} student"
+        else:
+            result = f"{phrase} {n} studentiv"
+    else:
+        result = f"{phrase} {n} studentiv"
+
+print(result)
