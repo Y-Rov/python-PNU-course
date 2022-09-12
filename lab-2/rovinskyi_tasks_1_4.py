51c4ecb6544319679f6b3e91688b879cf6548e22
VitalikPro13 <vitaliy2007rova@gmail.com>
2022-09-12 15:44:25 +0300

    Finished optional task №3

--- lab-2/rovinskyi_tasks_1_4.py
+++ lab-2/rovinskyi_tasks_1_4.py
@@ -1,5 +1,7 @@
+from lib2to3.pygram import Symbols
 from random import randrange
-# Task 1 - Create an empty list. Add items from the keyboard. Display the list. 
+# Task 1 
+# Create an empty list. Add items from the keyboard. Display the list. 
 # Display the length of the list. Remove the first element.
 
 first_list = []
@@ -11,7 +13,8 @@ print(f"Length = {len(first_list)}")
 first_list.pop(0)
 print(first_list)
 
-# Task 2 - Specify a list created by random numbers from 1 to 9 and print.
+# Task 2
+# Specify a list created by random numbers from 1 to 9 and print.
 # Add it to the end of the previous list
 
 second_list=[]
@@ -22,3 +25,12 @@ for i in range(n):
 print(second_list)
 first_list += second_list
 print(first_list)
+
+# Task 3
+# Enter a word from the keyboard.
+# Turn it into a list.
+
+word = input()
+symbols = list(word)
+print(symbols)
+
