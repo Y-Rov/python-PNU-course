024102eee7c890d834eab84aa680798fcb34efe8
VitalikPro13 <vitaliy2007rova@gmail.com>
2022-09-12 15:41:26 +0300

    Finished optional task №2

--- lab-2/rovinskyi_tasks_1_4.py
+++ lab-2/rovinskyi_tasks_1_4.py
@@ -1,11 +1,24 @@
+from random import randrange
 # Task 1 - Create an empty list. Add items from the keyboard. Display the list. 
 # Display the length of the list. Remove the first element.
 
-new_list = []
+first_list = []
 for i in range(int(input())):
-    new_list.append(int(input()))
+    first_list.append(int(input()))
 
-print(new_list)
-print(f"Length = {len(new_list)}")
-new_list.pop(0)
-print(new_list)
+print(first_list)
+print(f"Length = {len(first_list)}")
+first_list.pop(0)
+print(first_list)
+
+# Task 2 - Specify a list created by random numbers from 1 to 9 and print.
+# Add it to the end of the previous list
+
+second_list=[]
+n = int(input())
+for i in range(n):
+    second_list.append(randrange(1, 10))
+
+print(second_list)
+first_list += second_list
+print(first_list)
