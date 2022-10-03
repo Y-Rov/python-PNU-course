ad1683547974af7edea58f3dfe59b02e67a446d9
YuraRov <yura.rov555@gmail.com>
2022-10-03 20:52:41 +0300

    Finished task №3

--- lab-4/rovinskyi_tasks_1_7.py
+++ lab-4/rovinskyi_tasks_1_7.py
@@ -1,4 +1,7 @@
 # Task 1
+from typing import Dict
+
+
 def get_amount_of_one_letter_words(text: str) -> int:
     """Returns the quantity of one letter words."""
     return len(tuple(filter(lambda word: (len(word) == 1), text.split(' '))))
@@ -10,3 +13,11 @@ def get_amount_of_vowels(text: str) -> int:
     return len(tuple(filter(lambda symbol: (symbol.lower() in ('a', 'e', 'i', 'o', 'u')), text)))
 
 print(f'Quantity = {get_amount_of_vowels("Hello Python!")}')
+
+# Task 3
+def swap_keys_and_values(dictionary: dict) -> dict:
+    return dict([(value, key) for key, value in dictionary.items()])
+
+people = {"Adam": 30, "Bob": 12, "John": 34, "Jack": 45, "Liza": 22}
+people = swap_keys_and_values(people)
+print(people)
