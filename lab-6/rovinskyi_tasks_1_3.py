5d75ea334f09e248cfbf9725ea8494618308fa78
YuraRov <yura.rov555@gmail.com>
2022-10-25 19:28:18 +0300

    Finished task №1

--- lab-6/rovinskyi_tasks_1_3.py
+++ lab-6/rovinskyi_tasks_1_3.py
@@ -2,11 +2,12 @@ from math import sqrt
 from typing import NamedTuple
 
 class Coordinate(NamedTuple):
+    """A point in 2D space"""
     x: float
     y: float
 
 class Vector:
-
+    """Represents a geometric vector in Cartesian coordinate system"""
     def __init__(self, begin_point: Coordinate, end_point: Coordinate) -> None:
         self.begin_point = begin_point
         self.end_point = end_point
@@ -22,8 +23,19 @@ class Vector:
         return Vector(self.coordinate.x + other.coordinate.x, self.coordinate.y + other.coordinate.y)
     
     def length(self) -> float:
+        """Returns the length of a vector"""
         return sqrt(pow(self.coordinate.x, 2) + pow(self.coordinate.y, 2))
 
-vector1 = Vector(Coordinate(0, 0), Coordinate(1, 1))
-print(vector1.length())
+    def dot_product(self, other) -> float:
+        """Computes the dot product of two vectors"""
+        return self.coordinate.x * other.coordinate.x + self.coordinate.y * other.coordinate.y
+
+    def is_perpendicular_to(self, other) -> bool:
+        """Checks if two vectors are perpendicular"""
+        return self.dot_product(other) == 0
+
+vector1 = Vector(Coordinate(0, 0), Coordinate(0, 2))
+vector2 = Vector(Coordinate(0, 0), Coordinate(2, 0))
+
+print(vector1.is_perpendicular_to(vector2))
 
