d0f56c4033e19ec3f65b8fa867f145dcc9086609
YuraRov <yura.rov555@gmail.com>
2022-10-26 22:43:03 +0300

    Finished task №3 Almost finished the script documentation

--- lab-6/rovinskyi_tasks_1_3.py
+++ lab-6/rovinskyi_tasks_1_3.py
@@ -1,6 +1,6 @@
 import csv
 from math import sqrt
-from typing import Dict, List, NamedTuple
+from typing import Dict, List, NamedTuple, Tuple
 
 class Coordinate(NamedTuple):
     """A point in 2D space"""
@@ -37,10 +37,12 @@ class Vector:
 
 vector1 = Vector(Coordinate(0, 0), Coordinate(0, 2))
 vector2 = Vector(Coordinate(0, 0), Coordinate(2, 0))
-
 print(vector1.is_perpendicular_to(vector2))
 
 class Student:
+    """Represents a student with his name and a list of learning subjects"""
+    
+    # Type alias for marks
     marks_data = Dict[str, List[int]]
 
     def __init__(self, name: str, surname: str, marks: marks_data) -> None:
@@ -52,27 +54,44 @@ class Student:
         return f'Student - {self.name} {self.surname}; subjects with marks - {self.marks}.'
     
     def print_all_subjects(self) -> None:
+        """Prints all learning subjects"""
         print(list(self.marks.keys()))
 
     def get_marks_from(self, subject: str) -> List[int]:
+        """Returns all marks for the specific subject. 
+        If this subject doesn't exist - returns an empty list
+        """
         return self.marks.get(subject, [])
     
     def get_average_mark_from(self, subject: str) -> float:
+        """Computes and returns the average mark for the specific subject. 
+        If passed subject doesn't exist - returns 0
+        """
         marks = self.get_marks_from(subject)
         return sum(marks) / len(marks) if len(marks) != 0 else 0
 
     def get_total_average_mark(self) -> float:
+        """Computes and returns the average mark for all subjects"""
         all_sums_and_lengths = [(sum(marks), len(marks)) for marks in self.marks.values()]
         return sum(one_sum for one_sum, _ in all_sums_and_lengths) / sum(one_length for _, one_length in all_sums_and_lengths)
     
-    def has_credit_from(self, subject: str) -> bool:
+    def is_a_pass_in(self, subject: str) -> bool:
+        """Checks whether student got a pass in the specific subject"""
         return self.get_average_mark_from(subject) >= 4
 
     def add_new_subject_with_marks(self, subject_and_marks: marks_data) -> None:
+        """Adds a new subject with some marks"""
         self.marks.update(subject_and_marks)
     
     def add_new_mark_to(self, subject: str, mark: int) -> None:
-        self.get_marks_from(subject).append(mark)
+        """Adds a new mark to the specific subject. 
+        If subject doesn't exist - adds it also
+        """
+        marks = self.get_marks_from(subject)
+        if marks == []:
+            self.add_new_subject_with_marks({subject: [mark]})
+        else:
+            marks.append(mark)
     
     def get_list_of_average_marks(self) -> List[float]:
         subjects_and_avg_marks = {subject: sum(marks) / len(marks) for subject, marks in self.marks.items()}
@@ -82,26 +101,41 @@ class Student:
 
         return list(subjects_and_avg_marks.values())
 
-class ComputeSumFromCSV:
-    def __init__(self, file_path: str) -> None:
-        self.path = file_path
-    
-    def compute(self) -> None:
-        with open('KN-2.csv', 'r') as file:
-            csv_reader = csv.DictReader(file)
-            student_sum: Dict[str, float] = {}
-            for line in csv_reader:
-                total = 0
-                total += 2 if line['Lab1'] == '+' else 0
-                total += 2 if line['Lab2'] == '+' else 0
-                # total += if line['Lab3']
-
-
-
 student = Student('John', 'Doe', {'math': [3, 4, 5], 'physics': [2, 1, 3, 4]})
 student.print_all_subjects()
 print(student.get_marks_from('algebra'))
 print(student.get_average_mark_from('algebra'))
 print(student.get_total_average_mark())
-print(student.has_credit_from('physics'))
+print(student.is_a_pass_in('physics'))
 print(student.get_list_of_average_marks())
+
+class ComputeSumFromCSV:
+    def __init__(self, file_path: str) -> None:
+        self.path = file_path
+    
+    def compute(self) -> None:
+        with open(self.path, 'r') as in_file:
+            csv_reader = csv.DictReader(in_file)
+            student_sum: List[Tuple[str, float]] = []
+            for line in csv_reader:
+                total: float = 0
+                total += 2 if line['Lab1'] == '+' else 0
+                total += 2 if line['Lab2'] == '+' else 0
+                lab3_first_mark, lab3_second_mark = line['Lab3'].split('/')
+                total += float(lab3_first_mark) + float(lab3_second_mark)
+                total += int(float(line['Test']) / 5) + float(line['KR1'])
+                student_sum.append((line['Student'], total))
+            
+            self.data = student_sum
+    
+    def save_csv(self, path: str) -> None:
+        with open('Sum-KN-2.csv', 'w', newline='') as out_file:
+            writer = csv.writer(out_file)
+            writer.writerow(['Student', 'Sum'])
+            writer.writerows(self.data)   
+
+
+
+suma = ComputeSumFromCSV('KN-2.csv')
+suma.compute()
+a = 5
