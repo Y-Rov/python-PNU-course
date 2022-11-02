1c6af4ccf1e7eb2c70a2ce2b63685386a1d7c2b6
YuraRov <yura.rov555@gmail.com>
2022-11-02 19:11:57 +0200

    Finished part B of task №1

--- preparing-cw-1/script-1.py
+++ preparing-cw-1/script-1.py
@@ -1,8 +1,17 @@
 
 def count_different_digits_in(number: int) -> int:
-     return len(set(str(number)))
+    """Counts the amount of different digits in integer"""
+    return len(set(str(number)))
+
+def print_all_digits_that_not_in(number: int) -> None:
+    """Prints all digits thar are not presented in integer"""
+    digits = list(set('0123456789').difference(set(str(number))))
+    digits.sort()
+    print(digits)
+
 
 first_number: int = 133
 second_number: int = 12345
 print(count_different_digits_in(first_number))
 print(count_different_digits_in(second_number))
+print_all_digits_that_not_in(first_number)
